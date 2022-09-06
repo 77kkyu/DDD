aa37253c996c847ada94c5a5dba9210ca490f36b
77kkyu <77kkyu@gmail.com>
2022-09-06 21:32:08 +0900

    feat event

---
+++ src/main/java/com/example/dddstart/common/event/Event.java
@@ -0,0 +1,15 @@
+package com.example.dddstart.common.event;
+
+public abstract class Event {
+
+    private long timestamp;
+
+    public Event() {
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+}
