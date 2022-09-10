36be4e5c3c7cc4a55addf32a37c2e53db14d4fdc
77kkyu <77kkyu@gmail.com>
2022-09-10 21:01:33 +0900

    feat Password

---
+++ src/main/java/com/example/dddstart/member/domain/Password.java
@@ -0,0 +1,22 @@
+package com.example.dddstart.member.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Password {
+
+    @Column(name = "password")
+    private String value;
+
+    protected Password() {}
+
+    public Password(String value) {
+        this.value = value;
+    }
+
+    public boolean match(String password) {
+        return this.value.equals(password);
+    }
+
+}
