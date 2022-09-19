2b36ed2552f7527fb26cad39dea6ba750784d1d2
77kkyu <77kkyu@gmail.com>
2022-09-19 22:57:25 +0900

    setting

--- src/main/java/com/example/dddstart/DddStartApplication.java
+++ src/main/java/com/example/dddstart/DddStartApplication.java
@@ -1,8 +1,13 @@
 package com.example.dddstart;
 
+import com.example.dddstart.common.jpa.RangeableRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableJpaRepositories(repositoryBaseClass = RangeableRepositoryImpl.class)
+@EnableAsync
 @SpringBootApplication
 public class DddStartApplication {
 
