c85641c42d4077858ae65ac30d54a63b00a14fe6
77kkyu <77kkyu@gmail.com>
2022-10-08 00:04:03 +0900

    feat Product

---
+++ src/main/java/com/example/dddstart/category/domain/product/Option.java
@@ -0,0 +1,30 @@
+package com.example.dddstart.category.domain.product;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Option {
+
+	@Column(name = "option_value")
+	private String value;
+	@Column(name = "option_title")
+	private String title;
+
+	public Option() {
+	}
+
+	public Option(String value, String title) {
+		this.value = value;
+		this.title = title;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+}
