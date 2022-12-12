6266207dda2dea947cc5ecb4e7df79d5c959accf
77kkyu <77kkyu@gmail.com>
2022-12-13 01:20:12 +0900

    feat StartShippingRequest

---
+++ src/main/java/com/example/dddstart/order/application/StartShippingRequest.java
@@ -0,0 +1,22 @@
+package com.example.dddstart.order.application;
+
+public class StartShippingRequest {
+	private String orderNumber;
+	private long version;
+
+	protected StartShippingRequest() {
+	}
+
+	public StartShippingRequest(String orderNumber, long version) {
+		this.orderNumber = orderNumber;
+		this.version = version;
+	}
+
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+
+	public long getVersion() {
+		return version;
+	}
+}
